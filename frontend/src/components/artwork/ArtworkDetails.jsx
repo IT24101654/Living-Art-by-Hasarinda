d24708d34d9a5350f6657c8b1ed50c6a69072018
IT24101654 <yasith.hasarinda2003@gmail.com>
2026-03-31 01:12:13 +0530

    Update frontend/src/components/artwork/ArtworkDetails.jsx

---
+++ frontend/src/components/artwork/ArtworkDetails.jsx
@@ -0,0 +1,9 @@
+import CommissionSidebar from './CommissionSidebar'
+
+export default function ArtworkDetails() {
+  return (
+    <section className="section container narrow">
+      <CommissionSidebar />
+    </section>
+  )
+}
