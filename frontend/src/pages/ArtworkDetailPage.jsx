891a8e0a675bb710b35b25568ff28236786871bd
IT24101654 <yasith.hasarinda2003@gmail.com>
2026-03-31 01:12:15 +0530

    Update frontend/src/pages/ArtworkDetailPage.jsx

---
+++ frontend/src/pages/ArtworkDetailPage.jsx
@@ -0,0 +1,10 @@
+import PageLayout from '../components/layout/PageLayout'
+import ArtworkHero from '../components/artwork/ArtworkHero'
+
+export default function ArtworkDetailPage() {
+  return (
+    <PageLayout>
+      <ArtworkHero />
+    </PageLayout>
+  )
+}
